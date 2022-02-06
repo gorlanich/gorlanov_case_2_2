07df85cdb9a094ddfea976bef9d6608f3818397b
gorlanich <gorlanich@gmail.com>
2022-02-06 15:47:10 +0500

    Minor modifications: - added background - corrected container - corrected scroller

--- lib/main.dart
+++ lib/main.dart
@@ -28,65 +28,76 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
           body:Container(
+            //
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/bg.png"),
+            fit: BoxFit.cover,
+            )
+            ),
             width: double.infinity,
-            padding: EdgeInsets.symmetric(horizontal: 0),
-            child: Column(children: [
-              SizedBox(height:120,),
-              Image.asset("assets/images/logo.png"),
-              SizedBox(height:30,),
-              Text("Введите номер телефон", style: TextStyle(fontSize: 14,color: Color.fromRGBO(0, 0, 0, 0.4))),
-              SizedBox(height:30,),
-              SizedBox(width:300,
-                child: TextField(
-                  decoration: InputDecoration(
-                    enabledBorder: borderStyle,
-                    focusedBorder: borderStyle2,
-                    filled: true,
-                    fillColor: Color(0x5Edbebf6),
-                    labelText: "+7 xxx xxx xx xx",
+            height: double.infinity,
+            //padding: EdgeInsets.symmetric(horizontal: 0),
+            child: SingleChildScrollView(
+              child: Column(children: [
+                SizedBox(height:120,),
+                Image.asset("assets/images/logo.png"),
+                SizedBox(height:30,),
+                Text("Введите номер телефона", style: TextStyle(fontSize: 14,color: Color.fromRGBO(0, 0, 0, 0.4))),
+                SizedBox(height:30,),
+                SizedBox(width:300,
+                  child: TextField(
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(
+                      enabledBorder: borderStyle,
+                      focusedBorder: borderStyle2,
+                      filled: true,
+                      fillColor: Color(0x5Edbebf6),
+                      labelText: "+7 xxx xxx xx xx",
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height:30,),
-              const SizedBox(width:300,
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    enabledBorder: borderStyle,
-                    focusedBorder: borderStyle2,
-                    filled: true,
-                    fillColor: Color(0x5Edbebf6),
-                    labelText: "пароль",
+                SizedBox(height:30,),
+                const SizedBox(width:300,
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      enabledBorder: borderStyle,
+                      focusedBorder: borderStyle2,
+                      filled: true,
+                      fillColor: Color(0x5Edbebf6),
+                      labelText: "пароль",
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height:30,),
-              SizedBox(width:154, child:
-              ElevatedButton(onPressed: () {},  child: Text("Войти"),
-              style: ElevatedButton.styleFrom(
-                primary: Color(0xFF0079D0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
+                SizedBox(height:30,),
+                SizedBox(width:154, child:
+                ElevatedButton(onPressed: () {},  child: Text("Войти"),
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF0079D0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  )
                 )
-              )
-              ),
-              ),
-              SizedBox(height:80,),
-              InkWell(child: const Text("Регистрация", style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF0079D0),
-              ),
-              ), onTap: () {}),
-              SizedBox(height:10,),
-              InkWell(child: const Text("Забыли пароль?", style: TextStyle(
-                    fontSize: 16,
-                    color: Color(0xFF0079D0),
-                  ),
-              ), onTap: () {}),
-              SizedBox(height:100,),
-              Text("Case 2.2, author: Gorlanov AA"),
-            ],),
+                ),
+                ),
+                SizedBox(height:80,),
+                InkWell(child: const Text("Регистрация", style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF0079D0),
+                ),
+                ), onTap: () {}),
+                SizedBox(height:10,),
+                InkWell(child: const Text("Забыли пароль?", style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xFF0079D0),
+                    ),
+                ), onTap: () {}),
+                SizedBox(height:100,),
+                Text("Case 2.2, author: Gorlanov AA"),
+              ],),
+            ),
           )
 
         ),
